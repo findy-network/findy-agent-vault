77643ede456c25885ab7315d3037690389bac412
Laura Vuorenoja <laura.vuorenoja@op.fi>
2020-10-19 16:06:53 +0300

    Add authentication test.

--- server/server_test.go
+++ server/server_test.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -11,15 +10,9 @@ import (
 	tools "github.com/findy-network/findy-agent-api/tools/resolver"
 )
 
-type JSONError struct {
-	Message string   `json:"message"`
-	Path    []string `json:"path"`
-}
+var testToken string = createTestToken()
 
-type JSON struct {
-	Data   map[string]interface{} `json:"data"`
-	Errors *[]JSONError           `json:"errors"`
-}
+const testQuery = "{\n  __schema {\n    queryType {\n      name\n    }\n  }\n}"
 
 func queryJSON(content string) string {
 	content = strings.Replace(content, "\t", "", -1)
@@ -29,29 +22,43 @@ func queryJSON(content string) string {
 		}`
 }
 
-func doQuery(query string) (payload JSON) {
+func doQuery(query string, auth bool) (payload JSONPayload) {
 	request, _ := http.NewRequest(http.MethodPost, "/query", strings.NewReader(queryJSON(query)))
 	request.Header.Set("Content-Type", "application/json")
+	if auth {
+		request.Header.Set("Authorization", "Bearer "+testToken)
+	}
 	response := httptest.NewRecorder()
 
 	Server(&tools.Resolver{}).ServeHTTP(response, request)
 
 	bytes := response.Body.Bytes()
-	fmt.Println(string(bytes))
+	//fmt.Println(string(bytes))
 	_ = json.Unmarshal(bytes, &payload)
 	return
 }
 
+func doAuthQuery(query string) (payload JSONPayload) {
+	return doQuery(query, true)
+}
+
 func TestServerForError(t *testing.T) {
-	got := doQuery("{}")
+	got := doAuthQuery("{}")
 	if len(*got.Errors) == 0 {
 		t.Errorf("Expected errors, none found")
 	}
 }
 
+func TestServerForAuth(t *testing.T) {
+	got := doQuery(testQuery, false)
+	if len(*got.Errors) == 0 || (*got.Errors)[0].Extensions.Code != unauthenticated {
+		t.Errorf("Expected UNAUTHENTICATED error, none found")
+	}
+}
+
 func TestServerForSuccess(t *testing.T) {
-	got := doQuery("{\n  __schema {\n    queryType {\n      name\n    }\n  }\n}")
-	if _, ok := got.Data["__schema"]; !ok {
+	got := doAuthQuery(testQuery)
+	if _, ok := (*got.Data)["__schema"]; !ok {
 		t.Errorf("Expected response, none found")
 	}
 }
