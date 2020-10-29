d4bd300330ccae4fc6eab0de702d1d12a039a7f9
Laura Vuorenoja <laura.vuorenoja@op.fi>
2020-10-29 16:55:52 +0200

    Add invitation construction.

--- agency/findy.go
+++ agency/findy.go
@@ -3,15 +3,24 @@
 package agency
 
 import (
-	"bytes"
+	"encoding/json"
+	"fmt"
 	"os"
 
+	"github.com/golang/glog"
 	"github.com/lainio/err2"
 
-	"github.com/findy-network/findy-agent/cmds/onboard"
-
+	"github.com/findy-network/findy-agent/agent/cloud"
+	"github.com/findy-network/findy-agent/agent/mesg"
+	"github.com/findy-network/findy-agent/agent/pltype"
+	"github.com/findy-network/findy-agent/agent/ssi"
+	"github.com/findy-network/findy-agent/agent/utils"
+	"github.com/findy-network/findy-agent/client"
 	"github.com/findy-network/findy-agent/cmds"
 	"github.com/findy-network/findy-agent/cmds/agent"
+	"github.com/findy-network/findy-agent/cmds/onboard"
+	didexchange "github.com/findy-network/findy-agent/std/didexchange/invitation"
+	"github.com/findy-network/findy-wrapper-go/dto"
 )
 
 const (
@@ -20,7 +29,11 @@ const (
 	agencyURL  = "http://localhost:8080"
 )
 
-type Findy struct{}
+type Findy struct {
+	agent    *cloud.Agent
+	client   *client.Client
+	endpoint string
+}
 
 var Instance Agency = &Findy{}
 
@@ -29,6 +42,20 @@ func walletCmd() *cmds.Cmd {
 		WalletName: walletName,
 		WalletKey:  walletKey,
 	}
+}
+
+func (f *Findy) listenWs() {
+
+	glog.Info("starting listening loop")
+
+	go func() {
+		err2.Check(f.client.Listen(func(pl *mesg.Payload) (while bool, err error) {
+			defer err2.Return(&err)
+
+			fmt.Println(dto.ToJSON(pl))
+			return true, nil
+		}))
+	}()
 }
 
 // TODO: do not onboard here, instead use JWT for authentication to agency
@@ -55,28 +82,36 @@ func (f *Findy) Init() {
 		}
 	}
 
-	// ping
-	_, err = cmd.Exec(os.Stdout)
+	f.client = &client.Client{
+		Wallet: ssi.NewRawWalletCfg(walletName, walletKey),
+	}
+	f.agent = cloud.NewTransportReadyEA(f.client.Wallet)
+
+	f.client.SetAgent(f.agent)
+
+	im, err := f.agent.Trans().Call(pltype.CAPingOwnCA, &mesg.Msg{})
 	if err != nil {
 		panic(err)
 	}
+	f.endpoint = im.Message.Endpoint
+
+	f.listenWs()
 }
 
+// TODO: fetch constructed JSON from CA
 func (f *Findy) Invite() (invitation string, err error) {
 	defer err2.Return(&err)
 
-	cmd := agent.InvitationCmd{
-		Cmd:  *walletCmd(),
-		Name: walletName,
+	inv := didexchange.Invitation{
+		ID:              utils.UUID(),
+		Type:            pltype.AriesConnectionInvitation,
+		ServiceEndpoint: f.endpoint,
+		RecipientKeys:   []string{f.agent.Tr.PayloadPipe().Out.VerKey()},
+		Label:           walletName,
 	}
-	err = cmd.Validate()
-	err2.Check(err)
 
-	buf := new(bytes.Buffer)
-	_, err = cmd.Exec(buf)
-	err2.Check(err)
-
-	invitation = buf.String()
+	jsonBytes := err2.Bytes.Try(json.Marshal(&inv))
+	invitation = string(jsonBytes)
 	return
 }
 
