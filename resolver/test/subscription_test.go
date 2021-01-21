ecabac75d39d50079bab17706619797aba17e255
Laura Vuorenoja <laura.vuorenoja@op.fi>
2021-01-21 17:16:18 +0200

    Add tests for mutation and subscription resolvers.

---
+++ resolver/test/subscription_test.go
@@ -0,0 +1,17 @@
+package test
+
+import (
+	"testing"
+)
+
+func TestSubscribeEventAdded(t *testing.T) {
+	channel, err := r.Subscription().EventAdded(testContext())
+	if err != nil {
+		t.Errorf("Received unexpected error %s", err)
+	}
+	if channel == nil {
+		t.Errorf("Expecting result, received %v", channel)
+	}
+}
+
+// TODO: add test for sending events
