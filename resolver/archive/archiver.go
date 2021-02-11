97049bf248b8ca3484b5855870b664e64d52550e
Laura Vuorenoja <laura.vuorenoja@op.fi>
2021-02-11 20:22:44 +0200

    Use db archiver functionality from resolver.

--- resolver/archive/archiver.go
+++ resolver/archive/archiver.go
@@ -1,6 +1,8 @@
 package archive
 
 import (
+	"fmt"
+
 	agency "github.com/findy-network/findy-agent-vault/agency/model"
 	"github.com/findy-network/findy-agent-vault/db/model"
 	"github.com/findy-network/findy-agent-vault/db/store"
@@ -8,6 +10,7 @@ import (
 	"github.com/findy-network/findy-agent-vault/utils"
 	"github.com/golang/glog"
 	"github.com/lainio/err2"
+	"github.com/lainio/err2/assert"
 )
 
 type Archiver struct {
@@ -18,82 +21,163 @@ func NewArchiver(db store.DB) *Archiver {
 	return &Archiver{db}
 }
 
-func (a *Archiver) ArchiveConnection(info *agency.ArchiveInfo, data *agency.Connection) {
-	defer err2.Catch(func(err error) {
-		glog.Errorf("Encountered error when archiving connection %s", err)
-	})
+// TODO: write unit tests
+// TODO: should the archived flag be attached to the job instead of
+// protocol object itself?
+// TODO: should archived flag be visible somehow to the clients?
+// e.g. removing/arhiving of incomplete jobs from the UI
 
-	agent, err := a.db.GetAgent(nil, &info.AgentID)
+func (a *Archiver) matchProtocol(job *model.Job) (idToUpdate **string, archive func(string, string) error, err error) {
+	switch job.ProtocolType {
+	case graph.ProtocolTypeConnection:
+		idToUpdate = &job.ProtocolConnectionID
+		archive = a.db.ArchiveConnection
+	case graph.ProtocolTypeBasicMessage:
+		idToUpdate = &job.ProtocolMessageID
+		archive = a.db.ArchiveMessage
+	case graph.ProtocolTypeCredential:
+		idToUpdate = &job.ProtocolCredentialID
+		archive = a.db.ArchiveCredential
+	case graph.ProtocolTypeProof:
+		idToUpdate = &job.ProtocolProofID
+		archive = a.db.ArchiveProof
+	default:
+		return nil, nil, fmt.Errorf("invalid protocol type for job: %s", job.ProtocolType)
+	}
+
+	return idToUpdate, archive, nil
+}
+
+func (a *Archiver) archiveExisting(
+	info *agency.ArchiveInfo,
+	agent *model.Agent,
+	job *model.Job,
+) (err error) {
+	defer err2.Return(&err)
+
+	var (
+		idToUpdate **string
+		archive    func(string, string) error
+	)
+
+	utils.LogLow().Infof("Archive for existing job %s (%s)", job.ID, job.ProtocolType)
+
+	idToUpdate, archive, err = a.matchProtocol(job)
 	err2.Check(err)
 
-	utils.LogMed().Infof("Archiving connection %+v for tenant %s", data, agent.TenantID)
+	assert.P.True(*idToUpdate != nil, "existing job to archive should have a valid protocol id")
 
-	job, err := a.db.GetJob(info.JobID, agent.TenantID)
-	jobIsIncomplete := err == nil &&
-		(job.Status != graph.JobStatusComplete ||
-			job.Result != graph.JobResultSuccess ||
-			job.ProtocolConnectionID == nil)
+	// TODO: update data also?
+	err2.Check(archive(**idToUpdate, agent.TenantID))
 
-	if jobIsIncomplete {
-		// update connection
-		// TODO: update data also?
-		err = a.db.ArchiveConnection(info.ConnectionID, agent.TenantID)
-		err2.Check(err)
+	if job.Status != graph.JobStatusComplete || job.Result != graph.JobResultSuccess {
+		utils.LogLow().Infof("Update existing job %s (%s) when archiving", job.ID, job.ProtocolType)
 
 		// update job
 		job.Status = graph.JobStatusComplete
 		job.Result = graph.JobResultSuccess
 		job.ConnectionID = &info.ConnectionID
-		job.ProtocolConnectionID = &info.ConnectionID
 		_, err = a.db.UpdateJob(job)
 		err2.Check(err)
-	} else if store.ErrorCode(err) == store.NotExists {
-		now := utils.CurrentTime()
+	}
+	return nil
+}
 
-		// create connection
-		var connection *model.Connection
-		connection, err = a.db.AddConnection(model.NewConnection(info.ConnectionID, agent.TenantID, &model.Connection{
+func (a *Archiver) archiveNew(
+	info *agency.ArchiveInfo,
+	agent *model.Agent,
+	protocolType graph.ProtocolType,
+	addToStore func(*model.Agent, bool) (string, error),
+) (err error) {
+	defer err2.Return(&err)
+
+	id, err := addToStore(agent, info.InitiatedByUs)
+	err2.Check(err)
+
+	job := model.NewJob(info.JobID, agent.TenantID, &model.Job{
+		ConnectionID:  &info.ConnectionID,
+		ProtocolType:  protocolType,
+		InitiatedByUs: info.InitiatedByUs,
+		Status:        graph.JobStatusComplete,
+		Result:        graph.JobResultSuccess,
+	})
+	idToUpdate, _, err := a.matchProtocol(job)
+	*idToUpdate = &id
+
+	utils.LogLow().Infof("Create new job %s (%s) when archiving", job.ID, job.ProtocolType)
+
+	// add job
+	_, err = a.db.AddJob(job)
+	err2.Check(err)
+
+	return
+}
+
+func (a *Archiver) archive(
+	info *agency.ArchiveInfo,
+	protocolType graph.ProtocolType,
+	addToStore func(*model.Agent, bool) (string, error),
+) (err error) {
+	defer err2.Return(&err)
+
+	agent, err := a.db.GetAgent(nil, &info.AgentID)
+	err2.Check(err)
+
+	job, err := a.db.GetJob(info.JobID, agent.TenantID)
+
+	if err == nil { // job exists
+		err2.Check(a.archiveExisting(info, agent, job))
+	} else if store.ErrorCode(err) == store.NotExists { // job is new
+		err2.Check(a.archiveNew(info, agent, protocolType, addToStore))
+	} else {
+		err2.Check(err) // some other error
+	}
+
+	return
+}
+
+func (a *Archiver) ArchiveConnection(info *agency.ArchiveInfo, data *agency.Connection) {
+	defer err2.Catch(func(err error) {
+		glog.Errorf("Encountered error when archiving connection %s", err)
+	})
+
+	err2.Check(a.archive(info, graph.ProtocolTypeConnection, func(agent *model.Agent, initiatedByUs bool) (id string, err error) {
+		defer err2.Return(&err)
+
+		now := utils.CurrentTime()
+		connection, err := a.db.AddConnection(model.NewConnection(info.ConnectionID, agent.TenantID, &model.Connection{
 			OurDid:        data.OurDID,
 			TheirDid:      data.TheirDID,
 			TheirEndpoint: data.TheirEndpoint,
 			TheirLabel:    data.TheirLabel,
 			Approved:      &now, // TODO: get approved from agency
-			Invited:       job.InitiatedByUs,
+			Invited:       initiatedByUs,
 			Archived:      &now,
 		}))
 		err2.Check(err)
 
-		// add job
-		_, err = a.db.AddJob(model.NewJob(info.JobID, agent.TenantID, &model.Job{
-			ConnectionID:         &connection.ID,
-			ProtocolType:         graph.ProtocolTypeConnection,
-			ProtocolConnectionID: &connection.ID,
-			InitiatedByUs:        info.InitiatedByUs,
-			Status:               graph.JobStatusComplete,
-			Result:               graph.JobResultSuccess,
-		}))
-		err2.Check(err)
-	} else {
-		err2.Check(err)
-	}
+		return connection.ID, nil
+	}))
 }
 
 func (a *Archiver) ArchiveMessage(info *agency.ArchiveInfo, data *agency.Message) {
 	defer err2.Catch(func(err error) {
 		glog.Errorf("Encountered error when archiving message %s", err)
 	})
+	err2.Check(a.archive(info, graph.ProtocolTypeBasicMessage, func(agent *model.Agent, initiatedByUs bool) (id string, err error) {
+		defer err2.Return(&err)
 
-	agent, err := a.db.GetAgent(nil, &info.AgentID)
-	err2.Check(err)
-
-	utils.LogMed().Infof("Archiving message with connection id %s for tenant %s", info.ConnectionID, agent.TenantID)
-
-	/*	_, err = a.db.ArchiveMessage(model.NewMessage(agent.TenantID, &model.Message{
+		now := utils.CurrentTime()
+		message, err := a.db.AddMessage(model.NewMessage(agent.TenantID, &model.Message{
 			ConnectionID: info.ConnectionID,
 			Message:      data.Message,
 			SentByMe:     data.SentByMe, // TODO: sent time
+			Archived:     &now,
 		}))
-		err2.Check(err)*/
+		err2.Check(err)
+
+		return message.ID, nil
+	}))
 }
 
 func (a *Archiver) ArchiveCredential(info *agency.ArchiveInfo, data *agency.Credential) {
@@ -101,14 +185,11 @@ func (a *Archiver) ArchiveCredential(info *agency.ArchiveInfo, data *agency.Cred
 		glog.Errorf("Encountered error when archiving credential %s", err)
 	})
 
-	agent, err := a.db.GetAgent(nil, &info.AgentID)
-	err2.Check(err)
+	err2.Check(a.archive(info, graph.ProtocolTypeCredential, func(agent *model.Agent, initiatedByUs bool) (id string, err error) {
+		defer err2.Return(&err)
 
-	utils.LogMed().Infof("Archiving credential with connection id %s for tenant %s", info.ConnectionID, agent.TenantID)
-
-	/*	now := utils.CurrentTime()
-
-		_, err = a.db.ArchiveCredential(model.NewCredential(agent.TenantID, &model.Credential{
+		now := utils.CurrentTime()
+		credential, err := a.db.AddCredential(model.NewCredential(agent.TenantID, &model.Credential{
 			ConnectionID:  info.ConnectionID,
 			Role:          data.Role,
 			SchemaID:      data.SchemaID,
@@ -116,8 +197,12 @@ func (a *Archiver) ArchiveCredential(info *agency.ArchiveInfo, data *agency.Cred
 			Attributes:    data.Attributes,
 			InitiatedByUs: data.InitiatedByUs,
 			Issued:        &now, // TODO: get actual issued time
+			Archived:      &now,
 		}))
-		err2.Check(err)*/
+		err2.Check(err)
+
+		return credential.ID, nil
+	}))
 }
 
 func (a *Archiver) ArchiveProof(info *agency.ArchiveInfo, data *agency.Proof) {
@@ -125,21 +210,21 @@ func (a *Archiver) ArchiveProof(info *agency.ArchiveInfo, data *agency.Proof) {
 		glog.Errorf("Encountered error when archiving proof %s", err)
 	})
 
-	agent, err := a.db.GetAgent(nil, &info.AgentID)
-	err2.Check(err)
+	err2.Check(a.archive(info, graph.ProtocolTypeProof, func(agent *model.Agent, initiatedByUs bool) (id string, err error) {
+		defer err2.Return(&err)
 
-	utils.LogMed().Infof("Archiving proof with connection id %s for tenant %s", info.ConnectionID, agent.TenantID)
+		now := utils.CurrentTime()
+		proof, err := a.db.AddProof(model.NewProof(agent.TenantID, &model.Proof{
+			ConnectionID:  info.ConnectionID,
+			Role:          data.Role,
+			Attributes:    data.Attributes,
+			Result:        true,
+			InitiatedByUs: data.InitiatedByUs,
+			Verified:      &now, // TODO: get actual verified time
 
-	/*now := utils.CurrentTime()
+		}))
+		err2.Check(err)
 
-	_, err = a.db.ArchiveProof(model.NewProof(agent.TenantID, &model.Proof{
-		ConnectionID:  info.ConnectionID,
-		Role:          data.Role,
-		Attributes:    data.Attributes,
-		Result:        true,
-		InitiatedByUs: data.InitiatedByUs,
-		Verified:      &now, // TODO: get actual verified time
-
+		return proof.ID, nil
 	}))
-	err2.Check(err)*/
 }
