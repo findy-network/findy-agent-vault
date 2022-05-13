36d67b24d89fa65b49eef643c72f9dfd6f9099c9
Laura Vuorenoja <laura.vuorenoja@op.fi>
2022-05-13 15:02:17 +0300

    Wait for server start before tests are started.

--- scripts/wait-for-db.sh
+++ scripts/wait-for-db.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
+
+getServerStatus(){
+    local resCode=$(curl -s --write-out '%{http_code}' --output /dev/null http://$1:8085/health)
+    if (( ${resCode} == 200 )); then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# wait for db
 while ! nc -z $1 5432;
 do
     echo 'wait for db';
     sleep 1;
 done;
+
+# wait for vault
+NOW=${SECONDS}
+printf "Wait until server is up"
+while ! getServerStatus; do
+    printf "."
+    waitTime=$(($SECONDS - $NOW))
+    if (( ${waitTime} >= 60 )); then
+        printf "\nServer failed to start.\n"
+        exit 1
+    fi
+    sleep 1
+done
